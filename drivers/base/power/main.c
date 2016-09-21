9bec034f8f1276ff4c4845966a43fa830ebafad7
Tim Murray <timmurray@google.com>
2016-09-21 13:11:25 -0700

    cpuidle: don't disable cpuidle when entering suspend
    cpuidle was disabled while entering suspend as part of commit
    8651f97bd951d0bb1c10fa24e3fa3455193f3548 in order to work around some
    ACPI bugs. However, there's no reason to do this on modern
    platforms. Leaving cpuidle enabled can result in improved power
    consumption if dpm_resume_noirq runs for a significant time.
    
    Change-Id: Ie182785b176f448698c0264eba554d1e315e8a06
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -32,7 +32,6 @@
 #include <linux/suspend.h>
 #include <trace/events/power.h>
 #include <linux/cpufreq.h>
-#include <linux/cpuidle.h>
 #include <linux/devfreq.h>
 #include <linux/timer.h>
 #include <linux/wakeup_reason.h>
@@ -749,8 +748,6 @@ void dpm_resume_noirq(pm_message_t state)
 
 	resume_device_irqs();
 	device_wakeup_disarm_wake_irqs();
-
-	cpuidle_resume();
 }
 
 /**
@@ -1349,8 +1346,6 @@ static int dpm_noirq_suspend_devices(pm_message_t state)
 int dpm_suspend_noirq(pm_message_t state)
 {
 	int ret;
-
-	cpuidle_pause();
 
 	device_wakeup_arm_wake_irqs();
 	suspend_device_irqs();
